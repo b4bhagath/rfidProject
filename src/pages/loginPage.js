7d5025aa2c052e35b7af647137600b04d57238f2
Nikhil Bhagath <nikhilbhagath95@gmail.com>
2019-08-19 07:02:20 +0530

    login screen complete

--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -1,7 +1,18 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {Alert, Button, TextInput, View, StyleSheet, Image} from 'react-native';
+import {Alert, TextInput, View, StyleSheet, Image, Text} from 'react-native';
 
 export default class Login extends React.Component {
+  static navigationOptions = {
+    title: 'Login',
+    headerStyle: {
+      backgroundColor: '#45A6D9',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+  };
   constructor(props) {
     super(props);
 
@@ -32,25 +43,36 @@ export default class Login extends React.Component {
           />
           {/* </View> */}
           <View>
-            <TextInput
-              value={this.state.username}
-              onChangeText={username => this.setState({username})}
-              placeholder={'Username'}
-              style={styles.input}
-            />
-            <TextInput
-              value={this.state.password}
-              onChangeText={password => this.setState({password})}
-              placeholder={'Password'}
-              secureTextEntry={true}
-              style={styles.input}
-            />
-
-            <Button
-              title={'Login'}
-              style={styles.input}
-              onPress={this.onLogin.bind(this)}
-            />
+            <View style={styles.inputStyle}>
+              <TextInput
+                placeholderTextColor={'white'}
+                value={this.state.username}
+                onChangeText={username => this.setState({username})}
+                placeholder={'Username'}
+                style={styles.input}
+              />
+            </View>
+            <View style={styles.inputStyle}>
+              <TextInput
+                placeholderTextColor={'white'}
+                value={this.state.password}
+                onChangeText={password => this.setState({password})}
+                placeholder={'Password'}
+                secureTextEntry={true}
+                style={styles.input}
+              />
+            </View>
+            <View style={styles.loginButton} elevation={2}>
+              <Text style={styles.loginButtonText}>SIGN ON</Text>
+            </View>
+          </View>
+          <View style={styles.section3}>
+            <View>
+              <Text style={{color: 'white'}}> Help </Text>
+            </View>
+            <View>
+              <Text style={{color: 'white'}}>Languages</Text>
+            </View>
           </View>
         </View>
       </View>
@@ -71,12 +93,20 @@ const styles = StyleSheet.create({
     // justifyContent: 'flex-start'
   },
   input: {
-    width: 200,
+    width: 300,
     height: 44,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: 'black',
-    marginBottom: 10,
+    color: 'white',
+  },
+  loginButton: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    backgroundColor: '#fff',
+    display: 'flex',
+  },
+  loginButtonText: {
+    color: '#45A6D9',
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
   headerWrapper: {
     borderBottomColor: 'black',
@@ -86,11 +116,29 @@ const styles = StyleSheet.create({
   card: {
     borderRadius: 2,
     // display: 'inline-block',
-    height: 600,
+    backgroundColor: '#45A6D9',
+    height: 400,
     width: 350,
     display: 'flex',
     alignItems: 'center',
-    justifyContent: 'space-around',
+    justifyContent: 'flex-end',
     padding: 50,
+  },
+  inputStyle: {
+    borderBottomColor: 'white',
+    borderBottomWidth: 1,
+    // padding: 10,
+    marginBottom: 10,
+  },
+  inputButton: {
+    paddingTop: 20,
+    marginTop: 10,
+  },
+  section3: {
+    marginTop: 20,
+    // display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
   },
 });
